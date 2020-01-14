5f9a34887f8bdf29d8911bd2702d7a76579d0670
Wesley Ruedebusch <w.ruedebusch@gmail.com>
2020-01-14 16:44:37 -0500

    2/10

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here testing push
+const navBar = document.querySelector('.main-navigation');
+navBar.addEventListener('dblclick', () => {
+    navBar.style.backgroundColor = 'blue';
+    navBar.stopPropogation();
+})
+
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => {
+    button.addEventListener('contextmenu', () => {
+        button.style.backgroundColor = 'red';
+    })
+})
