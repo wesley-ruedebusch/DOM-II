1f817111ddafc439f37385687b1ac3485e167224
Wesley Ruedebusch <w.ruedebusch@gmail.com>
2020-01-14 20:00:01 -0500

    Mvp meet

--- js/index.js
+++ js/index.js
@@ -1,56 +1,60 @@
-const mouse = document.querySelector('.logo-heading');
-mouse.addEventListener("mouseover" , () => {
-mouse.style.transform = "scale(2.5)";
-mouse.style.transition = "all 0.5s";
-})
+const mouse = document.querySelector(".logo-heading");
+const links = document.querySelectorAll(".nav-link");
+const headerImg = document.querySelector(".intro img");
+const containerHeader = document.querySelector(".intro h2");
+const textHeaders = document.querySelectorAll(".text-content h2");
+const contentDestinationImg = document.querySelector(
+  ".content-destination img"
+);
+const changeButtons = document.querySelectorAll(".btn");
+const canalRide = document.getElementsByTagName('img')
 
+mouse.addEventListener("mouseover", () => {
+  mouse.style.transform = "scale(2.5)";
+  mouse.style.transition = "all 0.5s";
+});
 
 mouse.addEventListener("mouseleave", () => {
-    mouse.style.transform ="scale(2.0)";
-    mouse.style.transition =  "all 2.0s";
-})
+  mouse.style.transform = "scale(2.0)";
+  mouse.style.transition = "all 2.0s";
+});
 
-const links = document.querySelectorAll('.nav-link');
 links.forEach(link => {
-    link.addEventListener('click', () => {
-        link.style.color = "dodgerblue";
-    });
+  link.addEventListener("click", () => {
+    link.style.color = "dodgerblue";
+  });
 });
 
-const headerImg = document.querySelector('.intro img'); 
 containerHeader.addEventListener("wheel", () => {
-    containerHeader.style.backgroundColor = "red";
+  containerHeader.style.backgroundColor = "red";
+});
+
+textHeaders.forEach(textHeader => {
+  textHeader.addEventListener("mousemove", () => {
+    textHeader.style.color = "green";
+  });
+});
+
+canalRide[2].addEventListener('click', () => {
+canalRide[2].style.transform = 'rotate(180deg)'
 })
 
-const containerHeader = document.querySelector('.intro h2');
-const textHeaders = document.querySelectorAll('.text-content h2');
-textHeaders.forEach(textHeader => {
-    textHeader.addEventListener('mousemove', () => {
-        textHeader.style.color = 'green';
-    });
+
+contentDestinationImg.addEventListener("contextmenu", e => {
+  e.preventDefault();
+  alert("Sorry, image can not be copied!!");
 });
 
-
-const contentDestinationImg = document.querySelector('.content-destination img');
-contentDestinationImg.addEventListener('contextmenu', e => {
-    e.preventDefault();
-    alert("Sorry, image can not be copied!!");
+window.addEventListener("load", () => {
+  alert("Check out all the great vacations we offer!");
 });
 
-
-window.addEventListener('load', () => {
-    alert('Check out all the great vacations we offer!');
+window.addEventListener("resize", () => {
+  alert("you have resized the window");
 });
 
-
-window.addEventListener('resize', () => {
-    alert('you have resized the window');
-});
-
-
-const changeButtons = document.querySelectorAll('.btn'); 
 changeButtons.forEach(button => {
-    button.addEventListener('dblclick', () => {
-        alert('Great Selection!');
-    });
+  button.addEventListener("dblclick", () => {
+    alert("Great Selection!");
+  });
 });
